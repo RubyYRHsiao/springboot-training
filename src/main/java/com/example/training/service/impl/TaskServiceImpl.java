6784e7f8859e84c60294342eb8092e94d7df6221
Yun <momo1425360@gmail.com>
2023-03-05 14:54:03 +0800

    Add service & serviceImpl

--- src/main/java/com/example/training/service/impl/TaskServiceImpl.java
+++ src/main/java/com/example/training/service/impl/TaskServiceImpl.java
@@ -1,21 +1,42 @@
 package com.example.training.service.impl;
 
-import com.example.training.dao.TaskDao;
+import com.example.training.repository.TaskRepository;
 import com.example.training.entity.Task;
 import com.example.training.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class TaskServiceImpl implements TaskService {
 
     @Autowired
-    private TaskDao taskDao;
+    private TaskRepository taskRepository;
 
     @Override
     public List<Task> getAllTasks() {
-        return taskDao.getAllTasks();
+        return taskRepository.findAll();
+    }
+
+    @Override
+    public Optional<Task> findTaskById(Long id) {
+        return taskRepository.findById(id);
+    }
+
+    @Override
+    public Task saveTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public Task updateTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+        taskRepository.deleteById(id);
     }
 }
