f2055b203fc358d156cd834e3c8362b04f46813b
yun <2212032@exchange.prince.com.tw>
2023-03-07 14:49:12 +0800

    Add validation to entity

--- src/main/java/com/example/training/entity/Task.java
+++ src/main/java/com/example/training/entity/Task.java
@@ -1,6 +1,9 @@
 package com.example.training.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,9 +22,13 @@ public class Task {
     @Column(name = "task_id")
     private Long id;
 
+    @NotBlank
+    @Size(max = 20, message = "Name cant not be more than 20 characters")
     @Column(name = "name")
     private String name;
 
+    @NotNull
+    @Builder.Default
     @Column(name = "is_complete")
-    private Boolean isComplete;
+    private Boolean isComplete = false;
 }
