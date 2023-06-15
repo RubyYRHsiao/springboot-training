ced0b178b7d2b7ba52d801ae4268803169efe915
Yun <momo1425360@gmail.com>
2023-06-15 21:38:18 +0800

    Add basic settings of swagger

---
+++ src/main/java/com/example/training/config/SwaggerConfig.java
@@ -0,0 +1,8 @@
+package com.example.training.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    // TODO - Add swagger config
+}
